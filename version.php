77b7579b180f746b564615f33710b99938e665d2
entwicklung <entwicklung@Blendeds-MacBook-Pro.local>
2016-11-02 18:20:56 +0100

    update print layout

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016110101;
+$plugin->version   = 2016110201;
 $plugin->requires  = 2015050500;
 $plugin->component = 'theme_htwchur';
 $plugin->dependencies = array(
