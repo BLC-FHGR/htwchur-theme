225d89feb0cac43c09c740cd44ed2401da6394a8
Hanna Kummel <hanna.kummel@htwchur.ch>
2017-04-04 17:07:44 +0200

    Fix: Width of charts in analysis

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2017032803	;
+$plugin->version   = 2017040401	;
 $plugin->requires  = 2015050500;
 $plugin->component = 'theme_htwchur';
 $plugin->dependencies = array(
